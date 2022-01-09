f2288b438410d61ba0bc2dca59c08c6311a2bd11
DayvonAllen <dayvonallen93@outlook.com>
2022-01-09 10:20:46 -0600

    added notes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const electron = require('electron');
-// "ffmpeg" is a command line tool used to work with video and audio files
+// "ffmpeg" is a command line tool used to work with video and audio files, must install on your computer
 // can convert video files, can merge audio and video files.
 // can use it to query for the duration of a video file
 // "fluent-ffmpeg" makes working with "ffmpeg" easier with node.js, it's just a wrapper around "ffmpeg"
@@ -14,6 +14,7 @@ app.on('ready', () => {
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
+// main window IPC
 ipcMain.on('video:submit', (event, path) => {
   // ffprobe, reads a files metadata
   ffmpeg.ffprobe(path, (err, metadata) => {
