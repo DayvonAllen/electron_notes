459c818bdef6b0388a063c7ba30c9b056ab96cff
darkwin <darkwin@pop-os.localdomain>
2022-01-16 07:34:55 -0600

    added notes

--- offscreen_rendering_electron/main.js
+++ offscreen_rendering_electron/main.js
@@ -6,6 +6,8 @@ const fs = require('fs')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+// disable GPU usage when rendering images or content and uses the CPU instead
+// should use the GPU rendering if you have 3D CSS animations, if you are not using 3D animations you should use CPU rendering
 app.disableHardwareAcceleration()
 
 // Create a new BrowserWindow when `app` is ready
@@ -20,6 +22,7 @@ function createWindow () {
       // 'contextIsolation' defaults to "true" as from Electron v12
       contextIsolation: false,
       nodeIntegration: true,
+      // renders content onto a browser window on a second thread, it's not visible(renders the web content offscreen).
       offscreen: true
     }
   })
@@ -28,6 +31,8 @@ function createWindow () {
   mainWindow.loadURL('https://electronjs.org')
 
   let i = 1
+  // get rendered content, dirty - size and bounds of the area rendered, image - a native image instance of the rendered content
+  // this fires each and everytime the content rendered is changed
   mainWindow.webContents.on('paint', (e, dirty, image) => {
 
     let screenshot = image.toPNG()
